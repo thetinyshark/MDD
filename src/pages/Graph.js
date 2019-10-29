560b41d21a39a5ac9440f639bdc3e134b16e9104
bunnubb <bunnubb@BENEDICT-LUI.localdomain>
2019-10-29 23:29:37 +0800

    created graph component

---
+++ src/pages/Graph.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { withRouter, Link } from "react-router-dom";
+// import { FirebaseContext } from '../components/Firebase';
+import firebase from "../components/Firebase/firebase";
+import Plot from 'react-plotly.js';
+
+class Graph extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+         
+        };
+      }
+
+    componentDidMount() {
+        console.log("hello");
+        const y = [];
+        const timestamps = [];
+        const measureRef = firebase.database().ref("measures");
+        console.log(measureRef);
+        // measureRef.on("value", function (snapshot) {
+        //   console.log('hello')
+        //   var hello = snapshot.val()
+        //   console.log(hello)
+        // });
+        firebase
+          .database()
+          .ref("/measures")
+          .once("value")
+          .then(function(snapshot) {
+            y.push(snapshot.val().sensorValue);
+            timestamps.push(snapshot.val().timestamp); 
+    
+            // console.log(Object.keys(snapshot.val()).length);
+            // snapshot.forEach((child) => {
+            //   console.log(child.key, child.val());
+            //   y.push(child.val());
+    
+            });
+            
+            
+          });
+      }
+
+    render() {
+      return (
+        <Plot
+          data={[
+            {
+              x: {timestamps},
+              y: {y},
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: {color: 'red'},
+            },
+            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+          ]}
+          layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+        />
+      );
+    }
+  }
--- src/pages/Measure.jsx
+++ src/pages/Measure.jsx
@@ -31,42 +31,13 @@ import { array } from "prop-types";
 // );
 
 class Measure extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
       name: "Cherie", //change to username???how??
       heartratebpm: 92 //to read from database??
     };
-  }
-
-  componentDidMount() {
-    console.log("hello");
-    const y = [];
-    const timestamps = [];
-    const measureRef = firebase.database().ref("measures");
-    console.log(measureRef);
-    // measureRef.on("value", function (snapshot) {
-    //   console.log('hello')
-    //   var hello = snapshot.val()
-    //   console.log(hello)
-    // });
-    firebase
-      .database()
-      .ref("/measures")
-      .once("value")
-      .then(function(snapshot) {
-        y.push(snapshot.val().sensorValue);
-        timestamps.push(snapshot.val().timestamp); 
-
-        console.log(Object.keys(snapshot.val()).length);
-        snapshot.forEach((child) => {
-          console.log(child.key, child.val());
-          y.push(child.val());
-
-        });
-        
-        
-      });
   }
 
   render() {
@@ -136,23 +107,5 @@ class Measure extends Component {
   }
 }
 
-class Graph extends Component {
-  render() {
-    return (
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
-      />
-    );
-  }
-}
+
 export default withRouter(Measure);
