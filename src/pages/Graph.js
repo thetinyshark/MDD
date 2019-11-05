0d04eb13cf76e5c78c91726a9b10cf216c7d659c
bunnubb <bunnubb@BENEDICT-LUI.localdomain>
2019-11-06 02:30:33 +0800

    graph plotting added

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { withRouter, Link } from "react-router-dom";
-import { FirebaseContext } from '../components/Firebase';
+//import { withRouter, Link } from "react-router-dom";
+//import { FirebaseContext } from '../components/Firebase';
 //import firebase from 'firebase';
 import firebase from "../components/Firebase/firebase";
 import Plot from 'react-plotly.js';
@@ -11,68 +11,83 @@ export default class Graph extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            timestamps:[100,200,300],
-            sensorValues:[2,4,6],
-            heartratebpm: 0
+          
+            timestamp:[],
+            sensorValue:[],
+            heartratebpm: [],
+            layout: {datarevision:0, width: 600, height: 500, title: 'ECG plot'},
+            revision: 0
          
         };
       }
 
       componentDidMount() {
         console.log("mounted firebase");
-        const measureRef = firebase.database().ref("measures");
+        const measureRef = firebase.database().ref('measures');
         console.log(measureRef);
         // measureRef.on("value", function (snapshot) {
         //   console.log('hello')
         //   var hello = snapshot.val()
         //   console.log(hello)
         // });
-        firebase
-          .database()
-          .ref("/measures")
-          .once("value")
-          .then(function(snapshot) {
-            this.setState({
-                sensorValues: snapshot.val().sensorValue,
-                timestamps: snapshot.val().timestamp
-                });   
-            
-            // sensorValues.push(snapshot.val().sensorValue);
-            // timestamps.push(snapshot.val().timestamp); 
-    
-            // console.log(Object.keys(snapshot.val()).length);
-            // snapshot.forEach((child) => {
-            //   console.log(child.key, child.val());
-            //   y.push(child.val());
-    
-            }); 
-        setInterval(() => this.forceUpdate(), 10);
+        measureRef.on("value", snapshot => {
+          let node = snapshot.val();
+          console.log(snapshot.val());
+          const node_keys = Object.keys(node).map((key) => node[key]);
+          console.log(node_keys.length);
+          console.log("sensorValue is ", node_keys[node_keys.length - 1].sensorValue);
+          console.log("timestamp is", node_keys[node_keys.length - 1].timestamp);
+
+          var sensorValue_read = node_keys[node_keys.length - 1].sensorValue;
+          var timestamp_read = node_keys[node_keys.length - 1].timestamp;
+          var heartratebpm_read = node_keys[node_keys.length - 1].heartbpm;
+
+          const { timestamp, sensorValue, heartratebpm, layout} = this.state;
+          timestamp.push(timestamp_read);
+          sensorValue.push(sensorValue_read);
+          heartratebpm.push(heartratebpm_read);
+
+          if(timestamp.length >= 500) {
+            timestamp.shift();
+            sensorValue.shift();
+          }
+
+          this.setState( {
+            revision: this.state.revision + 1 })   
+          layout.datarevision = this.state.revision + 1;
+          });
+           
+        setInterval(() => this.forceUpdate(), 1);
         };
+      
 
 
     render() {
 
-        var data = {
-           // date = new.Date(),
-            sensorValues: this.sensorValues
-        };
-        
+      var dataplot = {
+        date: new Date(),
+        sensorValue: this.sensorValue,
+      };
+
+      console.log(this.state.timestamp);
+      // this.state.data.x = this.state.timestamp;
+      // console.log(this.state.data.x);
+      // this.state.data.y = this.state.sensorValue;
+      // console.log(this.state.data.y)
+
       return (
+
         // <RTChart
-        //   field = {['sensorValues']}
-        //   data = {data} />
+        //   chart = {chart}
+        //   field = {['sensorValue']}
+        //   data = {dataplot} />
         <Plot
-          data={[
-            {
-              x: this.timestamps,
-              y: this.sensorValues,
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: {color: 'red'},
-            },
-            {type: 'line', x: this.timestamps, y: [4,5,6]},
-          ]}
-          layout={{width: 320, height: 240, title: 'ECG plot'}}
+          data={[ {x: this.state.timestamp, y: this.state.sensorValue,
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'} }] }
+          layout={this.state.layout}
+          revision = {this.state.revision}
         />
       );
     }
